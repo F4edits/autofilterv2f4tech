910bd7bff0d0c3901857547730cd3299017f4ae3
F4edits <88608818+F4edits@users.noreply.github.com>
2021-09-04 17:11:38 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,12 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>കടന്ന് വര്യാ.....നോക്കി നിന്ന് സമയം കളയണ്ട...നൊമിനെ വേറെ ഗ്രൂപ്പിൽ ആഡ് ചെയ്യാൻ കഴിയില്ല
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+support us 🔥 @GKCCU098
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@Cinemapyscho @GKmoviesdownload6
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,10 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@Cinemapyscho @GKmoviesdownload6
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Cinemapyscho">Developer</a></i></b>
 
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
-"""
